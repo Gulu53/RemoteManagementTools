c12e03afadeaebad3d02fc3a5a6e1671d021e823
haohaolin1988 <haohaolin1988@gmail.com>
2013-08-15 04:27:37 +0000

    Flask response in JSON data structure.

--- FlaskCore.py
+++ FlaskCore.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-from flask import Flask
+from flask import Flask, Response
 import subprocess
+import json
 
 app =  Flask(__name__)
 
@@ -8,38 +9,58 @@ app =  Flask(__name__)
 def restart_all():
     init();
     subprocess.call('../RemoteManagementTools/Rrestart.py', shell=True)
-    return check();
+    resp = Response(response=check(),
+		    status=200,
+		    mimetype="application/json")
+    return resp;
 
 @app.route('/Restart/<reader>')
 def restart_reader(reader):
     init();
     subprocess.call('../RemoteManagementTools/Rrestart.py -r {0}'.format(reader), shell=True)
-    return check();
+    resp = Response(response=check(),
+		    status=200,
+		    mimetype="application/json")
+    return resp;
 
 @app.route('/Restart/<reader>/<process>')
 def restart_reader_proc(reader, process):
     init();
     subprocess.call('../RemoteManagementTools/Rrestart.py -r {0} --ap {1}'.format(reader, process), shell=True)
-    return check();
+    resp = Response(response=check(),
+		    status=200,
+		    mimetype="application/json")
+    return resp;
 
 @app.route('/Reboot/<reader>')
 def reboot_reader(reader):
     init();
     subprocess.call('../RemoteManagementTools/Rrestart.py -r {0} --reboot True'.format(reader), shell=True)
-    return check();
+    resp = Response(response=check(),
+		    status=200,
+		    mimetype="application/json")
+    return resp;
 
 @app.route('/Reboot')
 def reboot_all():
     init();
     subprocess.call('../RemoteManagementTools/Rrestart.py --reboot True'.format(reader), shell=True)
-    return check();
+    resp = Response(response=check(),
+		    status=200,
+		    mimetype="application/json")
+    return resp;
 
 def check():
+    result = {}
     ret = int(subprocess.check_output('cat ../RemoteManagementTools/Rrestart.log | grep -i error | wc -l', shell=True).rstrip('\n'))  
     if (ret):
-        return ('ERROR '+ subprocess.check_output('cat ../RemoteManagementTools/Rrestart.log | grep -i error', shell=True))
+        result["Result"] = "[-]ERROR"
+        result["DebugLog"] = subprocess.check_output('cat ../RemoteManagementTools/Rrestart.log | grep -i error', shell=True).split('\n')
+        return json.dumps(result, indent=4, separators=(',', ': '))
     else:
-        return ('SUCCESS ' + subprocess.check_output('cat ../RemoteManagementTools/Rrestart.log', shell=True))
+        result["Result"] = "[+]PASS"
+        result["DebugLog"] = subprocess.check_output('cat ../RemoteManagementTools/Rrestart.log', shell=True)
+        return json.dumps(result, indent=4, separators=(',', ': '))
 
 def init():
     subprocess.call('bash ../clearRemoteLog.sh', shell=True)
